220f86a82881374083f4966f00637f59a9cf1e70
gri0ta <146214932+gri0ta@users.noreply.github.com>
2024-05-08 20:08:22 +0300

    particles and car

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -5,32 +5,54 @@ using UnityEngine;
 public class Vehicle : MonoBehaviour
 {
 
-    public float speed = 10f;
+    public float maxSpeed = 10f;
+    public float maxReverseSpeed = 5f;
+    public float acceleration = 5f;
+    public float brakeAcceleration = 5f;
     public float turnSpeed = 10f;
     AudioSource engineSound;
+    //public ParticleSystem driftParticles;
+    public GameObject driftParticle;
     public Rigidbody rb;
-    public AudioSource source;
+    
+    public AnimationCurve pitchCurve;
+    public Vector3 localVelocity; //local velocity yra kaip mato musu masina is priekio
 
     private void Start()
     {
-        source = GetComponent<AudioSource>();
+        engineSound = GetComponent<AudioSource>();
+        //driftParticles = gameObject.GetComponent<ParticleSystem>();
     }
     public void Update()
     {
         print(rb.velocity.magnitude * 3.6f);
-        engineSound.pitch = 1 + rb.velocity.magnitude * 0.1f;
+        var speedRatio = rb.velocity.magnitude / maxSpeed;
+        engineSound.pitch = pitchCurve.Evaluate(speedRatio);
+
+        var localVelocity = transform.InverseTransformVector(rb.velocity);
+
+        if (localVelocity.x < -1 || localVelocity.x > 1 )
+        {
+            //driftParticles.Play();
+            driftParticle.SetActive(true);
+            print("drift");
+        }
+
+        //DRAG
+        rb.velocity += transform.forward * localVelocity.z * 0.1f * Time.deltaTime;
+        rb.velocity += transform.right * localVelocity.x * 1f * Time.deltaTime;
     }
 
     public void Brake()
     {
-
+        if (localVelocity.z < maxReverseSpeed) return; //velocity gali but minusinis
+        rb.velocity += transform.forward * -brakeAcceleration * Time.deltaTime;
     }
 
     public void Gas()
-    {
-        print("Gas");
-        //rb.velocity = transform.forward *speed;
-        rb.velocity += transform.forward * speed * Time.deltaTime;
+    { 
+        if (localVelocity.z > maxSpeed) return;
+        rb.velocity += transform.forward * acceleration * Time.deltaTime;
     }
 
     public void Turn(float amount)
